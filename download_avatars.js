b08fd3b166b2584eb36c6b7fa1c92d34d2e11331
vagrant <stan.solomean@gmail.com>
2019-01-16 21:13:53 +0000

    loging all avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,21 +1,42 @@
 
 
 var request = require('request');
+var token = require('./secrets.js');
 
 console.log('weclome');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
+  var options = {
+  url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  headers : {
+    "User-Agent" : 'token'
+    }
+  };
 
+
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+
+    var result = [];
+    var parsedBod = JSON.parse(body);
+
+
+    for (var i = 0; i < parsedBod.length; i++){
+      result.push(parsedBod[i].avatar_url);
+    }
+
+
+    console.log(result);
+
+  });
 
 }
 
-repo: {
-
-}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Result:", result);
 });
