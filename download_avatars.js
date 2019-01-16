579eed0f364cce6639394f4fc33f3e17af975791
vagrant <stan.solomean@gmail.com>
2019-01-16 20:00:28 +0000

    minore addition

--- download_avatars.js
+++ download_avatars.js
@@ -2,4 +2,20 @@
 
 var request = require('request');
 
-console.log('weclome')
+console.log('weclome');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+
+
+}
+
+repo: {
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
