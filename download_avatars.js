05676274f61375b1b61c1a96fc5992cb21b5d7f7
vagrant <stan.solomean@gmail.com>
2019-01-20 22:07:32 +0000

    added comments on code

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,10 @@
 
+//Dependencies
+const request = require('request');
+const fs = require('fs');
+const token = require('./secrets.js');
 
-var request = require('request');
-var fs = require('fs');
-var token = require('./secrets.js');
-
-
-
+//main function that will stream files into a deignated path
 function downloadImageByURL(url, filePath) {
   request.get(url)
        .on('error', function (err) {
@@ -21,8 +20,10 @@ function downloadImageByURL(url, filePath) {
        .pipe(fs.createWriteStream(filePath));
 }
 
+//a function that will obtain repo owner and repo name, and will use those arguments to
+//access the images
 function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
+  const options = {
   url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
   headers : {
     "User-Agent" : 'token'
@@ -30,20 +31,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
   request(options, function(err, res, body) {
     cb(err, body);
-    var parsedBod = JSON.parse(body);
-    for (var i = 0; i < parsedBod.length; i++){
+    const parsedBod = JSON.parse(body);
+    for (let i = 0; i < parsedBod.length; i++){
       downloadImageByURL(parsedBod[i].avatar_url, `./avatars/${parsedBod[i].login}.jpg`);
     }
   });
 }
 
+//letting user input repo owner and name through command line
 let rOwner = process.argv[2];
 let rName = process.argv[3];
 
+//error message if repo owner or name have not been provided
 if (rOwner === undefined || rName === undefined) {
   console.log("Error - repo owner or repo name have not been given!")
 }
 
+//main function call
 getRepoContributors(rOwner, rName, function(err, result) {
   console.log("Errors:", err);
 });
